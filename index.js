773a845e5ccb55face6dc73f4da45e4f29cae9bd
lewis karanu <lewiskaranu0@gmail.com>
2024-03-25 16:54:06 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-// Write your code here
+const num1 = 2
+ const num2 = 31
+ const multiply = num1 * num2
+ 
+ let random = Math.floor(Math.random() * 10) +1;
+ console.log(random);
+ 
+ const num3 = 29
+ const num4 = 5 
+ const mod = num3 % num4
+ mod === 4 
+ console.log(mod)
+ const max =Math.max(10,20)
+ console.log(max)
+ 
